bd99e2993064ec1581e48ed55086a14a78a43483
tahakenar <emirtahakenar@hotmail.com>
2024-02-18 23:31:32 +0300

    test: add fixed test data & fix: add missing deleteFile line

--- tests/wc_test.cpp
+++ tests/wc_test.cpp
@@ -9,19 +9,17 @@
 const std::string TEST_FILENAME = "test.txt";
 const std::string TEST_DATA = "line1\nline2\nline3\nline4 secondWord\n";
 
-// TODO (emir.kenar): Give actual values
-const unsigned int NUM_OF_BYTES = 1;
-const unsigned int NUM_OF_LINES = 1;
-const unsigned int NUM_OF_WORDS = 1;
-const unsigned int NUM_OF_CHARS = 1;
+const unsigned int NUM_OF_BYTES = 35;
+const unsigned int NUM_OF_LINES = 4;
+const unsigned int NUM_OF_WORDS = 5;
+const unsigned int NUM_OF_CHARS = 35;
 
-// TODO (emir.kenar): Give actual values
-const std::string NUM_OF_BYTES_OUT = "";
-const std::string NUM_OF_LINES_OUT = "";
-const std::string NUM_OF_WORDS_OUT = "";
-const std::string NUM_OF_CHARS_OUT = "";
-const std::string NO_OPTIONS_OUT = "";
-const std::string NO_FILENAME_OUT = "";
+const std::string NUM_OF_BYTES_OUT = "      35 " + TEST_FILENAME;
+const std::string NUM_OF_LINES_OUT = "       4 " + TEST_FILENAME;
+const std::string NUM_OF_WORDS_OUT = "       5 " + TEST_FILENAME;
+const std::string NUM_OF_CHARS_OUT = "      35 " + TEST_FILENAME;
+const std::string NO_OPTIONS_OUT = "       4       5      35 " + TEST_FILENAME;
+const std::string NO_FILENAME_OUT = "       4       5      35";
 
  
 
@@ -29,6 +27,7 @@ const std::string NO_FILENAME_OUT = "";
 void writeToTestFile(const std::string& filename, const std::string& data) {
     std::ofstream ofs;
     ofs.open(filename);
+    ofs << data;
     ofs.close();
 }
 
@@ -89,6 +88,7 @@ TEST(WcTest, GetNumberOfCharsStdOut) {
     auto out = wc.getWcOutput();
 
     EXPECT_EQ(out, NUM_OF_CHARS_OUT);
+    deleteTestFileIfExists(TEST_FILENAME);
 }
 
 TEST(WcTest, NoOptionsProvidedStdOut) {
@@ -98,6 +98,7 @@ TEST(WcTest, NoOptionsProvidedStdOut) {
     auto out = wc.getWcOutput();
 
     EXPECT_EQ(out, NO_OPTIONS_OUT);
+    deleteTestFileIfExists(TEST_FILENAME);
 }
 
 TEST(WcTest, NoFilenameProvidedStdOut) {
